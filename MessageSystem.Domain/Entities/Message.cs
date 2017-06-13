528065a286269d9c2f5c56fffeb2522ff7c61153
NeilEarlam <neil.earlam123@gmail.com>
2017-06-13 01:20:21 +0100

    Added Entities and Factory

---
+++ MessageSystem.Domain/Entities/Message.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageSystem.Domain.Entities
+{
+    public enum MessageType
+    {
+        SMS,
+        Twitter
+    }
+
+    public abstract class MessageBase
+    {
+        public string Id { get; set; }
+        public string Sender { get; set; }
+        public string Body { get; set; }
+        public int MaxSize { get; set; }
+        public MessageType Type { get; set; }
+    }
+
+    public class SmsMessage : MessageBase
+    {
+        public SmsMessage()
+        {
+            Id = string.Empty;
+            Sender = string.Empty;
+            Body = string.Empty;
+            MaxSize = 140;
+            Type = MessageType.SMS;
+        }
+    }
+
+    public class TwitterMessage : MessageBase
+    {
+        public TwitterMessage()
+        {
+            Id = string.Empty;
+            Sender = string.Empty;
+            Body = string.Empty;
+            MaxSize = 140;
+            Type = MessageType.Twitter;
+        }
+    }
+}
